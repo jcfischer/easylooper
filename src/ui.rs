313a5735f2392c0621642ede51ac44a9fddf1dcb
Jens-Christian Fischer <jens-christian.fischer@switch.ch>
2018-06-13 23:12:25 +0200

    Use new font features in UI

--- src/ui.rs
+++ src/ui.rs
@@ -1,7 +1,8 @@
 use tinyui::*;
 
+
 const WIDTH: f64 = 480.;
-const HEIGHT: f64 = 320.;
+const HEIGHT: f64 = 160.;
 
 #[derive(Clone, Copy)]
 pub struct PluginWindow {
@@ -13,17 +14,16 @@ pub struct PluginWindow {
 
 impl EventHandler for PluginWindow {
     fn handle(&mut self, event: Event) {
-
         println!("-- event {:?}", event);
         match event {
             Event::ButtonClicked(name) => {
                 match name.as_str() {
-                    "a button" => {self.button.set_text("clicked me"); },
+                    "a button" => { self.button.set_text("clicked me"); }
                     _ => {}
                 }
             }
             // Event::WindowWillClose => App::quit(), // don't do this on a vst
-            Event::WindowWillClose => {},
+            Event::WindowWillClose => {}
             _ => (),
         }
     }
@@ -31,28 +31,34 @@ impl EventHandler for PluginWindow {
 
 impl PluginWindow {
     pub fn new(mut window: Window) -> Self {
-        let window_rect = Rect::new(0., 0., HEIGHT, WIDTH);
+        let window_rect = Rect::new(0., 0., WIDTH, HEIGHT);
         let (_top_half_rect, bottom_half_rect) = window_rect.split_horizontal();
         // info!("building window");
         let mut app = Self {
             window: window,
-            counter: Label::new ( "0:00", Rect::new( 10., 10., 16., 80. )),
-            state_label: Label::new ( "Stopped", Rect::new( 10., 10., 48., 80. )),
+            counter: Label::new("0:00", Rect::new(10., 10., 120., 32.)),
+            state_label: Label::new("Stopped", Rect::new(10., 44., 120., 32.)),
             button: ButtonBuilder {
                 id: "a button",
                 text: "click me",
                 style: ButtonStyle::Square,
-                position: bottom_half_rect.inset(10.)
+                position: bottom_half_rect.inset(10.),
             }.build(),
         };
 
-        window.set_title("Easy Looper");
+        window.set_title("Echo Looper");
         window.set_background_color(Color::system_gray());
         // app.button.attach(&mut app.window);
+
+        let font = Font::init("Menlo", 24.);
+        app.counter.set_font(font);
+        app.state_label.set_font(font);
+
         app.counter.attach(&mut app.window);
         app.state_label.attach(&mut app.window);
         app.window.set_handler(app.clone());
 
         app
     }
+
 }
